0d75c10af0db90a04ee6e826fc0c6cbbd8c4e42b
Aubrey Graves <grav0165@gmail.com>
2023-07-16 11:48:14 -0500

    Cleaned up survey pages, finished admin page stretch.

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -2,16 +2,10 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import SendIcon from '@mui/icons-material/Send';
-import TextField from '@mui/material/TextField';
 import './Review.css'
 import { useHistory } from 'react-router-dom';
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
 import axios from 'axios';
 
